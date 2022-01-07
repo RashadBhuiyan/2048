e6569516463e591191eea0599aa6f0a3f74191e9
Rashad Ahmed Bhuiyan <rashadabhuiyan@gmail.com>
2022-01-07 16:28:46 -0500

    Added all files relating to the 2048 project
    Included in the files are some testing files used to ensure that the game runs smoothly with minimal errors.

---
+++ Direction.java
@@ -0,0 +1,10 @@
+package src;
+
+/**
+ * @file: Direction.java
+ * @Author: Rashad Ahmed Bhuiyan - bhuiyr2
+ * @Date: 2021-04-12
+ * @Description: defines enumerated type for directions
+ */
+
+public enum Direction { Up, Down, Left, Right;}
